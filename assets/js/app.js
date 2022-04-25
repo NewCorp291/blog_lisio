23c132052bde3bc49e3344963b35f962fa61a433
Administrator <admin@example.com>
2022-04-25 10:32:44 +0200

    dashboard article

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,4 @@
 import '../css/app.scss';
-const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
-require('bootstrap');
-
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
 
 import {Dropdown} from 'bootstrap';
 
@@ -24,4 +12,5 @@ const enableDropdowns = () => {
         return new Dropdown(dropdownToggleEl)
     });
 }
+
 
